4762a4d1cd80a6a8af5dcfa0a2c1359cb6ff8af2
โจ้ <136960019+KOBENFF@users.noreply.github.com>
2025-06-14 15:22:45 +0700

    Update AttackHub.lua

--- AttackHub.lua
+++ AttackHub.lua
@@ -1,1 +1,1 @@
- game.Players.LocalPlayer:Kick("Script Update Pls Join Discord for get new version") setclipboard("https://discord.gg/HFerRpdp3w")
+ game.Players.LocalPlayer:Kick("Script Update Pls Join Discord for get new version|เวอร์ชั่นใหม่เอาได้ในดิส") setclipboard("https://discord.gg/HFerRpdp3w")
